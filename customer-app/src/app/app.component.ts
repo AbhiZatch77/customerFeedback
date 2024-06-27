b578176cba1683b15970a8b2edd305486a9a11ca
Abhishek <abhishek200men@gmail.com>
2024-06-27 19:46:02 +0530

    Feedback form basic UI.

---
+++ customer-app/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule,NgIf],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  feedback = {
+    name: '',
+    email: '',
+    rating: 5,
+    phone: '',
+    message: ''
+  };
+
+  constructor(private http: HttpClient) { }
+
+  submitFeedback() {
+    console.log('Feedback submitted successfully!');
+    // Assuming your backend endpoint is at '/api/feedback'
+    // this.http.post('/api/feedback', this.feedback)
+    //   .subscribe(
+    //     response => {
+    //       console.log('Feedback submitted successfully!', response);
+    //       //reset form
+    //       //this.feedback = { name: '', email: '', phone: '', rating: 5, message: '' };
+    //     },
+    //     error => {
+    //       console.error('Error submitting feedback:', error);
+    //     }
+    //   );
+  }
+}
